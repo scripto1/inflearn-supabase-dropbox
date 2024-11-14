a7b869bd49952474a68293cbf0b5f727d7028ade
scripto1 <heeyongi@gmail.com>
2024-11-14 18:26:40 +0900

    feat: 프로젝트완료

--- components/DragnDropZone.tsx
+++ components/DragnDropZone.tsx
@@ -1,32 +1,60 @@
 "use client";
 
-import { Button } from "@material-tailwind/react";
+import { Button, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
 import { uploadFile } from "actions/storageActions";
-import { useRef } from "react";
+import { queryClient } from "app/config/ReactQueryClientProvider";
+import { useCallback, useRef } from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function DragnDropZone() {
   const fileRef = useRef(null);
+  const uploadImageMutation = useMutation({
+    mutationFn: uploadFile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["images"],
+      });
+    },
+  });
+
+  const onDrop = useCallback(async (acceptedFiles) => {
+    if (acceptedFiles.length > 0) {
+      const formData = new FormData();
+
+      acceptedFiles.forEach((file) => {
+        formData.append(file.name, file);
+      });
+
+      const result = await uploadImageMutation.mutate(formData);
+      console.log(result);
+    }
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    multiple: true,
+  });
 
   return (
-    <form
-      onSubmit={async (event) => {
-        event.preventDefault();
-        const file = fileRef.current.files?.[0];
-        if (file) {
-          const formData = new FormData();
-          formData.append("file", file);
-          const result = await uploadFile(formData);
-          console.log(result);
-        }
-      }}
-      className="flex flex-col items-center justify-center w-full rounded-2xl py-24 border-2 border-dotted border-deep-purple-200 bg-light-green-100"
+    <div
+      {...getRootProps()}
+      className="flex flex-col items-center justify-center w-full rounded-2xl py-24 border-2 border-dotted border-deep-purple-200 bg-light-green-100 cursor-pointer"
     >
-      <input ref={fileRef} type="file" className="" />
-      <p className="font-bold text-green-700">
-        파일을 여기에 끌어다 놓거나 클릭하여 업로드하세요.
-      </p>
+      <input {...getInputProps()} />
+      {uploadImageMutation.isPending ? (
+        // @ts-ignore
+        <Spinner />
+      ) : isDragActive ? (
+        <p className="font-bold text-green-700">파일을 여기에 놓아주세요</p>
+      ) : (
+        <p className="font-bold text-green-700">
+          파일을 여기에 끌어다 놓거나 클릭하여 업로드하세요.
+        </p>
+      )}
       {/* @ts-ignore */}
-      <Button type="submit">파일 업로드</Button>
-    </form>
+      {/* <Button loading={uploadImageMutation.isPending} type="submit">
+        파일 업로드
+      </Button> */}
+    </div>
   );
 }
--- components/DropBoxImage.tsx
+++ components/DropBoxImage.tsx
@@ -1,14 +1,21 @@
 import { Trash } from "@phosphor-icons/react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteFile } from "actions/storageActions";
+import { queryClient } from "app/config/ReactQueryClientProvider";
 import { getImageUrl } from "utils/supabase/storage";
 
 export default function DropBoxImage({ image }) {
+  const deleteFileMutation = useMutation({
+    mutationFn: deleteFile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["images"],
+      });
+    },
+  });
+
   return (
     <figure className="relative w-full flex flex-col border border-gray-200 rounded-lg shadow-lg p-3 gap-2">
-      <Trash
-        size={32}
-        color="white"
-        className="absolute bg-blue-gray-400 rounded-md top-5 right-5 p-1 cursor-pointer"
-      />
       <picture>
         <img
           src={getImageUrl(image.name)}
@@ -16,6 +23,14 @@ export default function DropBoxImage({ image }) {
         />
       </picture>
       <figcaption>{image.name}</figcaption>
+      <Trash
+        onClick={() => {
+          deleteFileMutation.mutate(image.name);
+        }}
+        size={32}
+        color="white"
+        className="absolute bg-blue-gray-400 rounded-md top-5 right-5 p-1 cursor-pointer"
+      />
     </figure>
   );
 }
